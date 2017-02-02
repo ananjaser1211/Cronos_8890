83fd4e0c5da75314e0377ea828fdf6e921fcf3cc
AndreiLux <luxneb@gmail.com>
2017-02-02 16:27:33 +0100

    opp: De-obscurify OPP structs and disclose find_device_opp Move definitions of OPP structs in the header file so that its members are public and the struct is no longer obscure.
    Define interface for find_device_opp in the header file.
    
    Signed-off-by: morogoku <morogoku@hotmail.com>

--- drivers/base/power/opp.c
+++ drivers/base/power/opp.c
@@ -57,6 +57,7 @@
  *
  * This structure stores the OPP information for a given device.
  */
+#if 0 /* Definition moved to header file */
 struct dev_pm_opp {
 	struct list_head node;
 
@@ -67,7 +68,7 @@ struct dev_pm_opp {
 	struct device_opp *dev_opp;
 	struct rcu_head head;
 };
-
+#endif
 /**
  * struct device_opp - Device opp structure
  * @node:	list node - contains the devices with OPPs that
@@ -83,6 +84,7 @@ struct dev_pm_opp {
  * a device. This structure is not meant to be shared to users as it is
  * meant for book keeping and private to OPP library
  */
+#if 0 /* Definition moved to header file */
 struct device_opp {
 	struct list_head node;
 
@@ -90,7 +92,7 @@ struct device_opp {
 	struct srcu_notifier_head head;
 	struct list_head opp_list;
 };
-
+#endif
 /*
  * The root of the list of all devices. All device_opp structures branch off
  * from here, with each device_opp containing the list of opp it supports in
@@ -114,7 +116,7 @@ static DEFINE_MUTEX(dev_opp_list_lock);
  * is a RCU protected pointer. This means that device_opp is valid as long
  * as we are under RCU lock.
  */
-static struct device_opp *find_device_opp(struct device *dev)
+struct device_opp *find_device_opp(struct device *dev)
 {
 	struct device_opp *tmp_dev_opp, *dev_opp = ERR_PTR(-ENODEV);
 
