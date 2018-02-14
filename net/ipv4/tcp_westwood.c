04ba593cb50c3f6f587f83dc7bcbb2a7e4597185
Planet-X <planetx.dev@gmail.com>
2018-02-14 20:26:28 +0100

    tcp_westwood: Remove CONFIG_HZ dependency
    Default to HZ=1000
    
    Signed-off-by: Planet-X <planetx.dev@gmail.com>

--- net/ipv4/tcp_westwood.c
+++ net/ipv4/tcp_westwood.c
@@ -43,8 +43,8 @@ struct westwood {
 };
 
 /* TCP Westwood functions and constants */
-#define TCP_WESTWOOD_RTT_MIN   (HZ/18)	/* 55.55ms */
-#define TCP_WESTWOOD_INIT_RTT  (18*HZ)	/* maybe too conservative?! */
+#define TCP_WESTWOOD_RTT_MIN   (55)	/* For HZ=1000: HZ/18 = 55.55ms */
+#define TCP_WESTWOOD_INIT_RTT  (18000)	/* For HZ=1000: HZ*18 = 18000 | maybe too conservative?! */
 
 /*
  * @tcp_westwood_create
