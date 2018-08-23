4de1d33750ba459a60278b475fef1dbd2a1f7caf
DevriesL <therkduan@gmail.com>
2018-08-23 21:57:11 +0400

    mm: swap: set vnswap as fast device

--- drivers/staging/vnswap/vnswap.c
+++ drivers/staging/vnswap/vnswap.c
@@ -1060,6 +1060,7 @@ static int create_device(struct vnswap *vnswap)
 	vnswap->disk->private_data = vnswap;
 	snprintf(vnswap->disk->disk_name, 16, "vnswap%d", 0);
 
+	__set_bit(QUEUE_FLAG_FAST, &vnswap->queue->queue_flags);
 	/* Actual capacity set using sysfs (/sys/block/vnswap<id>/disksize) */
 	set_capacity(vnswap->disk, 0);
 
