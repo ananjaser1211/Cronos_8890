6682ba91b00879234254fbd9730afb1dd5819b34
morogoku <morogoku@hotmail.com>
2019-07-10 20:50:53 +0200

    import N935FXXU4CSC4 OSRC
    Signed-off-by: morogoku <morogoku@hotmail.com>

--- fs/proc/meminfo.c
+++ fs/proc/meminfo.c
@@ -109,6 +109,12 @@ static int meminfo_proc_show(struct seq_file *m, void *v)
 #ifndef CONFIG_MMU
 		"MmapCopy:       %8lu kB\n"
 #endif
+#ifdef CONFIG_RBIN
+		"RbinTotal:      %8lu kB\n"
+		"RbinAllocated:  %8d kB\n"
+		"RbinPool:       %8d kB\n"
+		"RbinFree:       %8lu kB\n"
+#endif
 		"SwapTotal:      %8lu kB\n"
 		"SwapFree:       %8lu kB\n"
 		"Dirty:          %8lu kB\n"
@@ -161,6 +167,12 @@ static int meminfo_proc_show(struct seq_file *m, void *v)
 #endif
 #ifndef CONFIG_MMU
 		K((unsigned long) atomic_long_read(&mmap_pages_allocated)),
+#endif
+#ifdef CONFIG_RBIN
+		K(totalrbin_pages),
+		K(atomic_read(&rbin_allocated_pages) + atomic_read(&rbin_pool_pages)),
+		K(atomic_read(&rbin_pool_pages)),
+		K(global_page_state(NR_FREE_RBIN_PAGES)),
 #endif
 		K(i.totalswap),
 		K(i.freeswap),
