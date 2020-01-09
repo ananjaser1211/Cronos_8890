3779a4e0f164a7e781e28b530bea9b99760ba056
Anan jaser <ananjaser@gmail.com>
2020-01-10 01:42:55 +0400

    cronos: uprev

--- cronos.sh
+++ cronos.sh
@@ -38,7 +38,7 @@ CR_KERNEL=$CR_DIR/arch/arm64/boot/Image
 # Compiled dtb by dtbtool
 CR_DTB=$CR_DIR/boot.img-dtb
 # Kernel Name and Version
-CR_VERSION=V2.5
+CR_VERSION=V2.6
 CR_NAME=HeliosKernel
 # Thread count
 CR_JOBS=$(nproc --all)
