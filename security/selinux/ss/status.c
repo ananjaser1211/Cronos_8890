82cd6b35d781d8e2ab5bc674bf57a8ea5296ab90
AnanJaser <ananjaser@gmail.com>
2020-02-06 09:31:29 +0400

    Revert "selinux: mega switch"
    This reverts commit e5ff8d8ad48107810a353a643b26620cb5152283.

--- security/selinux/ss/status.c
+++ security/selinux/ss/status.c
@@ -59,10 +59,8 @@ struct page *selinux_kernel_status_page(void)
 			status->version = SELINUX_KERNEL_STATUS_VERSION;
 			status->sequence = 0;
 // [ SEC_SELINUX_PORTING_COMMON
-#if defined(CONFIG_SECURITY_SELINUX_ALWAYS_ENFORCE)
- 			status->enforcing = 1;
-#elif defined(CONFIG_SECURITY_SELINUX_NEVER_ENFORCE)
-			status->enforcing = 0;
+#ifdef CONFIG_ALWAYS_ENFORCE
+			status->enforcing = 1;
 #else
 			status->enforcing = selinux_enforcing;
 #endif
