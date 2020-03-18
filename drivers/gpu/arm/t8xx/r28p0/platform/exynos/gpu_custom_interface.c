92bca8c033969b56865e46fcbd7c2b6efd1f515d
morogoku <morogoku@hotmail.com>
2020-03-18 21:15:28 +0100

    gpu: add a gpu driver version variable
    - shows a string with the correct version of the Mali GPU driver in this file:
    
    /sys/kernel/gpu/gpu_driver_version
    
    by morogoku
    
    Signed-off-by: morogoku <morogoku@hotmail.com>

--- drivers/gpu/arm/t8xx/r28p0/platform/exynos/gpu_custom_interface.c
+++ drivers/gpu/arm/t8xx/r28p0/platform/exynos/gpu_custom_interface.c
@@ -2262,6 +2262,11 @@ static ssize_t set_kernel_sysfs_governor(struct kobject *kobj, struct kobj_attri
 }
 #endif
 
+static ssize_t show_kernel_sysfs_gpu_driver_version(struct kobject *kobj, struct kobj_attribute *attr, char *buf)
+{
+	return scnprintf(buf, PAGE_SIZE, "%s\n", MALI_RELEASE_NAME);
+}
+
 static ssize_t show_kernel_sysfs_gpu_model(struct kobject *kobj, struct kobj_attribute *attr, char *buf)
 {
 	/* COPY from mali_kbase_core_linux.c : 2595 line, last updated: 20180102, r21p0-01rel0 */
@@ -2390,6 +2395,9 @@ static struct kobj_attribute gpu_clock_attribute =
 static struct kobj_attribute gpu_freq_table_attribute =
 	__ATTR(gpu_freq_table, S_IRUGO, show_kernel_sysfs_freq_table, NULL);
 
+static struct kobj_attribute gpu_driver_version_attribute =
+	__ATTR(gpu_driver_version, S_IRUGO, show_kernel_sysfs_gpu_driver_version, NULL);
+	
 static struct kobj_attribute gpu_model_attribute =
 	__ATTR(gpu_model, S_IRUGO, show_kernel_sysfs_gpu_model, NULL);
 
@@ -2407,6 +2415,7 @@ static struct attribute *attrs[] = {
 	&gpu_busy_attribute.attr,
 	&gpu_clock_attribute.attr,
 	&gpu_freq_table_attribute.attr,
+	&gpu_driver_version_attribute.attr,
 	&gpu_model_attribute.attr,
 	NULL,
 };
--- drivers/gpu/arm/t8xx/r29p0/platform/exynos/gpu_custom_interface.c
+++ drivers/gpu/arm/t8xx/r29p0/platform/exynos/gpu_custom_interface.c
@@ -2262,6 +2262,11 @@ static ssize_t set_kernel_sysfs_governor(struct kobject *kobj, struct kobj_attri
 }
 #endif
 
+static ssize_t show_kernel_sysfs_gpu_driver_version(struct kobject *kobj, struct kobj_attribute *attr, char *buf)
+{
+	return scnprintf(buf, PAGE_SIZE, "%s\n", MALI_RELEASE_NAME);
+}
+
 static ssize_t show_kernel_sysfs_gpu_model(struct kobject *kobj, struct kobj_attribute *attr, char *buf)
 {
 	/* COPY from mali_kbase_core_linux.c : 2595 line, last updated: 20180102, r21p0-01rel0 */
@@ -2393,6 +2398,9 @@ static struct kobj_attribute gpu_freq_table_attribute =
 static struct kobj_attribute gpu_model_attribute =
 	__ATTR(gpu_model, S_IRUGO, show_kernel_sysfs_gpu_model, NULL);
 
+static struct kobj_attribute gpu_driver_version_attribute =
+	__ATTR(gpu_driver_version, S_IRUGO, show_kernel_sysfs_gpu_driver_version, NULL);
+
 
 static struct attribute *attrs[] = {
 #if defined(CONFIG_EXYNOS_THERMAL) && defined(CONFIG_GPU_THERMAL)
@@ -2407,6 +2415,7 @@ static struct attribute *attrs[] = {
 	&gpu_busy_attribute.attr,
 	&gpu_clock_attribute.attr,
 	&gpu_freq_table_attribute.attr,
+	&gpu_driver_version_attribute.attr,
 	&gpu_model_attribute.attr,
 	NULL,
 };
