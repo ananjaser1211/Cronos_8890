f8809c54dc96c165eaa4215c99b015a7beb8cd73
ananjaser1211 <ananjaser@gmail.com>
2020-06-05 21:16:08 +0400

    Revert "battery: revert sec_control mods"
    * This was not relevant to the overnight bug
    * store mode can be used to mitigate the issue

--- drivers/battery/max77854_charger.c
+++ drivers/battery/max77854_charger.c
@@ -1720,6 +1720,8 @@ static void max77854_enable_aicl_irq(struct max77854_charger_data *charger)
 	pr_info("%s enabled: %d\n", __func__, charger->irq_aicl_enabled);
 }
 
+bool unstable_power_detection = true;
+
 static void max77854_chgin_isr_work(struct work_struct *work)
 {
 	struct max77854_charger_data *charger = container_of(work,
@@ -1756,7 +1758,7 @@ static void max77854_chgin_isr_work(struct work_struct *work)
 			stable_count++;
 		else
 			stable_count = 0;
-		if (stable_count > 10) {
+		if (stable_count > 10 || !unstable_power_detection) {
 			pr_info("%s: irq(%d), chgin(0x%x), chg_dtls(0x%x) prev 0x%x\n",
 					__func__, charger->irq_chgin,
 					chgin_dtls, chg_dtls, prev_chgin_dtls);
