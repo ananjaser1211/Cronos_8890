7c372c3135b43eac60075d620e5274e978c98789
ananjaser1211 <ananjaser@gmail.com>
2020-06-25 22:40:25 +0400

    kernel: Add SEC_DEBUG_INIT_EXIT_PANIC
    * Exynos7870_QQ
    
    Signed-off-by: ananjaser1211 <ananjaser@gmail.com>

--- kernel/exit.c
+++ kernel/exit.c
@@ -882,6 +882,14 @@ do_group_exit(int exit_code)
 {
 	struct signal_struct *sig = current->signal;
 
+#ifdef CONFIG_SEC_DEBUG_INIT_EXIT_PANIC
+	if (current->pid == 1) {
+		pr_err("[%s] trap before init(1) group exit, exit_code:%d\n",
+			current->comm, exit_code);
+		panic("Attempted to kill init task group! exitcode=0x%08x\n", exit_code);
+	}
+#endif
+
 	BUG_ON(exit_code & 0x80); /* core dumps don't get here */
 
 	if (signal_group_exit(sig))
