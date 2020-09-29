6ca4c0c908c4962660abc742d0593aac26773cc8
Anan Jaser <ananjaser@gmail.com>
2020-09-29 12:19:52 +0400

    drivers: gpu: r29p0: fix misleading intendation

--- drivers/gpu/arm/t8xx/r29p0/backend/gpu/mali_kbase_device_hw.c
+++ drivers/gpu/arm/t8xx/r29p0/backend/gpu/mali_kbase_device_hw.c
@@ -217,7 +217,7 @@ static void kbase_report_gpu_fault(struct kbase_device *kbdev, int multiple)
     if (kbdev->vendor_callbacks->update_status)
         kbdev->vendor_callbacks->update_status(kbdev, "completion_code", status);
 
-	dev_warn(kbdev->dev, "GPU Fault 0x%08x (%s) at 0x%016llx",
+		dev_warn(kbdev->dev, "GPU Fault 0x%08x (%s) at 0x%016llx",
 			status & 0xFF,
 			kbase_exception_name(kbdev, status),
 			address);
