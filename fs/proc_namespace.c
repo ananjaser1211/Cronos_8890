2f56effbbe8907d0c39d8d2d0732b8d1a8c280ca
Jaroslav Zviezda <acroreiser@gmail.com>
2020-10-27 12:59:17 +0400

    HACK: fs/proc: block access to mounts for "IsolatedService"
    Prevents Magisk mount leakage and root detection by apps with unmodified detection code from here:
    https://darvincitech.wordpress.com/2019/11/04/detecting-magisk-hide/
    
    Example: https://play.google.com/store/apps/details?id=ua.gov.diia.app
    
    oryginal commit: acroreiser/Aconitum@d17c01f

--- fs/proc_namespace.c
+++ fs/proc_namespace.c
@@ -237,6 +237,12 @@ static int mounts_open_common(struct inode *inode, struct file *file,
 	if (!task)
 		goto err;
 
+	if(!strncmp("IsolatedService", task->comm, 15))
+	{
+		ret = -EINVAL;
+		goto err;
+	}
+
 	task_lock(task);
 	nsp = task->nsproxy;
 	if (!nsp || !nsp->mnt_ns) {
